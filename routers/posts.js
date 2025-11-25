7df8016f9100bd79928e66cc5ac8929c7defbbf6
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-25 12:44:26 +0100

    import CRUD in routers

--- routers/posts.js
+++ routers/posts.js
@@ -7,6 +7,34 @@ const router = express.Router()
 
 //la logica di cio che avviene
 
+router.get('/', (req, res) => {
+  res.send('OHH BENVENUTI nel nostro blog')
+})
+
+router.get('/posts', function (req, res) {
+res.send('Elenco con tutti i post');
+});
+// show
+router.get('/posts/:id', function (req, res) {
+res.send(`Dettagli del post ${req.params.id}`);
+});
+// store
+router.post('/posts', function (req, res) {
+res.send('Creazione di un nuovo post');
+});
+// update
+router.put('/posts/:id', function (req, res) {
+res.send(`Modifica totale tombale, definitiva del post ${req.params.id} `);
+});
+// modify
+router.patch('/posts/:id', function (req, res) {
+res.send(`Modifica piccolina del post ${req.params.id}`);
+});
+// destroy
+router.delete('/a posts/:id', (req, res)=>{
+  res.send(`Addio soldato: ${req.params.id}`)
+})
+
 
 //il mio return ()
 module.exports = router
