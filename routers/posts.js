b924c3b0a2e248640999705508ce9aaa87ba543b
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-25 12:42:16 +0100

    create routes folder

---
+++ routers/posts.js
@@ -0,0 +1,12 @@
+//il nostro import
+const express = require('express')
+
+//creazione della route per esposrtarla
+const router = express.Router()
+
+
+//la logica di cio che avviene
+
+
+//il mio return ()
+module.exports = router
